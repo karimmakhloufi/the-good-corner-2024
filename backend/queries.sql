f9e1a01afd34f2250922099750074169e2710b7e
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-20 01:09:03 +0200

    Created sqlite db with raw sql

---
+++ backend/queries.sql
@@ -0,0 +1,390 @@
+CREATE TABLE IF NOT EXISTS ad (
+	id INTEGER PRIMARY KEY,
+   	title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    author TEXT NOT NULL,
+    price REAL NOT NULL,
+    creation_date TEXT NOT NULL,
+    img_url TEXT NOT NULL,
+    city TEXT NOT NULL
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Bordeaux'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Bordeaux'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Bordeaux'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Bordeaux'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Bordeaux'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Paris'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
+
+INSERT INTO ad (
+    title,
+    description,
+    author,
+    price,
+    creation_date,
+    img_url,
+    city
+)
+VALUES (
+    'Bike',
+    'Bike in good condition',
+    'John',
+    100,
+    '2021-01-01',
+    'https://www.bike.com',
+    'Lyon'
+);
