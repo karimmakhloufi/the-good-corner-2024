d1e50e906d93ea2667b3ac719ab260b099460432
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-20 01:36:15 +0200

    PUT ads in db via rest api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,33 +7,6 @@ const port = 3000;
 
 app.use(express.json());
 
-let ads = [
-  {
-    id: 1,
-    title: "Bike to sell",
-    description:
-      "My bike is blue, working fine. I'm selling it because I've got a new one",
-    owner: "bike.seller@gmail.com",
-    price: 100,
-    picture:
-      "https://images.lecho.be/view?iid=dc:113129565&context=ONLINE&ratio=16/9&width=640&u=1508242455000",
-    location: "Paris",
-    createdAt: "2023-09-05T10:13:14.755Z",
-  },
-  {
-    id: 2,
-    title: "Car to sell",
-    description:
-      "My car is blue, working fine. I'm selling it because I've got a new one",
-    owner: "car.seller@gmail.com",
-    price: 10000,
-    picture:
-      "https://www.automobile-magazine.fr/asset/cms/34973/config/28294/apres-plusieurs-prototypes-la-bollore-bluecar-a-fini-par-devoiler-sa-version-definitive.jpg",
-    location: "Paris",
-    createdAt: "2023-10-05T10:14:15.922Z",
-  },
-];
-
 app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
@@ -73,13 +46,19 @@ app.delete("/ads/:id", (req, res) => {
 });
 
 app.put("/ads/:id", (req, res) => {
-  ads = ads.map((ad) => {
-    if (ad.id !== Number.parseInt(req.params.id)) {
-      return ad;
-    } else {
-      return req.body;
-    }
-  });
+  const stmt = db.prepare(
+    "UPDATE ad SET title = ?, description = ?, author = ?, price = ?, img_url = ?, city = ?, creation_date = ? WHERE id = ?"
+  );
+  stmt.run([
+    req.body.title,
+    req.body.description,
+    req.body.author,
+    req.body.price,
+    req.body.img_url,
+    req.body.city,
+    req.body.createdAt,
+    req.params.id,
+  ]);
   res.send("The ad was updated");
 });
 
