b451bdc699cafa1153c4b1aa376f8cac219f3387
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-21 20:06:11 +0200

    PUT Ad with typeorm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import "reflect-metadata";
 import express from "express";
-import sqlite3 from "sqlite3";
 import { dataSource } from "./config/db";
 import { Ad } from "./entities/Ad";
-const db = new sqlite3.Database("good_corner.sqlite");
 
 const app = express();
 const port = 3000;
@@ -30,20 +28,8 @@ app.delete("/ads/:id", async (req, res) => {
   res.send("The ad was deleted");
 });
 
-app.put("/ads/:id", (req, res) => {
-  const stmt = db.prepare(
-    "UPDATE ad SET title = ?, description = ?, author = ?, price = ?, img_url = ?, city = ?, creation_date = ? WHERE id = ?"
-  );
-  stmt.run([
-    req.body.title,
-    req.body.description,
-    req.body.author,
-    req.body.price,
-    req.body.img_url,
-    req.body.city,
-    req.body.createdAt,
-    req.params.id,
-  ]);
+app.put("/ads/:id", async (req, res) => {
+  await Ad.update({ id: parseInt(req.params.id) }, req.body);
   res.send("The ad was updated");
 });
 
