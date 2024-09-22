a10141ceabfbc7c61721eda54a0294dd988a66ae
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-22 15:20:17 +0200

    CRUD Category

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,11 @@
+import { Category } from "../entities/Category";
 import { Ad } from "../entities/Ad";
 import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "good_corner.sqlite",
-  entities: [Ad],
+  entities: [Ad, Category],
   synchronize: true,
   logging: ["query", "error"],
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import express from "express";
 import { dataSource } from "./config/db";
 import { Ad } from "./entities/Ad";
+import { Category } from "./entities/Category";
 
 const app = express();
 const port = 3000;
@@ -31,6 +32,27 @@ app.delete("/ads/:id", async (req, res) => {
 app.put("/ads/:id", async (req, res) => {
   await Ad.update({ id: parseInt(req.params.id) }, req.body);
   res.send("The ad was updated");
+});
+
+app.get("/categories", async (_req, res) => {
+  const result = await Category.find();
+  res.send(result);
+});
+
+app.post("/categories", async (req, res) => {
+  await Category.create(req.body).save();
+  console.log(req.body);
+  res.send("Category created");
+});
+
+app.delete("/categories/:id", async (req, res) => {
+  await Category.delete(req.params.id);
+  res.send("The ad was deleted");
+});
+
+app.put("/categories/:id", async (req, res) => {
+  await Category.update({ id: parseInt(req.params.id) }, req.body);
+  res.send("The category was updated");
 });
 
 app.listen(port, async () => {
