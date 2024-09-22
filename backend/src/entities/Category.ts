fe129561d1093c4ff209f27426df92bd74437af1
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-22 17:20:08 +0200

    Create and Edit ad with category

--- backend/src/entities/Category.ts
+++ backend/src/entities/Category.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Ad } from "./Ad";
 
 @Entity()
 export class Category extends BaseEntity {
@@ -7,4 +14,7 @@ export class Category extends BaseEntity {
 
   @Column()
   name: string;
+
+  @OneToMany(() => Ad, (ad) => ad.category)
+  ads: Ad[];
 }
