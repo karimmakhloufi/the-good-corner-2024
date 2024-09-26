00f2250992ccab4c5b8df03138c236d6a7be9ce5
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-27 00:23:02 +0200

    AdContainer and map on array object

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -1,62 +1,57 @@
+import AdCard, { AdCardProps } from "./AdCard";
+
 const RecentsAds = () => {
+  const ads: AdCardProps[] = [
+    {
+      title: "Table",
+      imgUrl: "/images/table.webp",
+      price: 120,
+      link: "/ads/table",
+    },
+    {
+      title: "Dame-jeanne",
+      imgUrl: "/images/dame-jeanne.webp",
+      price: 75,
+      link: "/ads/dame-jeanne",
+    },
+    {
+      title: "Vide-poche",
+      imgUrl: "/images/vide-poche.webp",
+      price: 4,
+      link: "/ads/vide-poche",
+    },
+    {
+      title: "Porte-magazine",
+      imgUrl: "/images/porte-magazine.webp",
+      price: 45,
+      link: "/ads/porte-magazine",
+    },
+    {
+      title: "Vaisselier",
+      imgUrl: "/images/vaisselier.webp",
+      price: 900,
+      link: "/ads/vaisselier",
+    },
+    {
+      title: "Bougie",
+      imgUrl: "/images/bougie.webp",
+      price: 8,
+      link: "/ads/bougie",
+    },
+  ];
   return (
     <>
       <h2>Annonces récentes</h2>
       <section className="recent-ads">
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/table">
-            <img className="ad-card-image" src="/images/table.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Table</div>
-              <div className="ad-card-price">120 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/dame-jeanne">
-            <img className="ad-card-image" src="/images/dame-jeanne.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Dame-jeanne</div>
-              <div className="ad-card-price">75 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/vide-poche">
-            <img className="ad-card-image" src="/images/vide-poche.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Vide-poche</div>
-              <div className="ad-card-price">4 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/vaisselier">
-            <img className="ad-card-image" src="/images/vaisselier.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Vaisselier</div>
-              <div className="ad-card-price">900 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/bougie">
-            <img className="ad-card-image" src="/images/bougie.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Bougie</div>
-              <div className="ad-card-price">8 €</div>
-            </div>
-          </a>
-        </div>
-        <div className="ad-card-container">
-          <a className="ad-card-link" href="/ads/porte-magazine">
-            <img className="ad-card-image" src="/images/porte-magazine.webp" />
-            <div className="ad-card-text">
-              <div className="ad-card-title">Porte-magazine</div>
-              <div className="ad-card-price">45 €</div>
-            </div>
-          </a>
-        </div>
+        {ads.map((el) => (
+          <AdCard
+            key={el.title}
+            title={el.title}
+            imgUrl={el.imgUrl}
+            price={el.price}
+            link={el.link}
+          />
+        ))}
       </section>
     </>
   );
