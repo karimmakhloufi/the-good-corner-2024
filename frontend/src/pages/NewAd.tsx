d12d9f7dbe14c8af85d6ae52c77266464e4bc506
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-09-29 15:53:27 +0200

    Added tags to ad creation form

--- frontend/src/pages/NewAd.tsx
+++ frontend/src/pages/NewAd.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
 type Category = { id: number; name: string };
 const NewAd = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [tags, setTags] = useState<
+    { id: number; name: string; checked: boolean }[]
+  >([]);
   useEffect(() => {
     const fetchCategories = async () => {
       const result = await axios.get<Category[]>(
@@ -11,6 +15,13 @@ const NewAd = () => {
       setCategories(result.data);
     };
     fetchCategories();
+    const fetchTags = async () => {
+      const tagsResult = await axios.get<{ id: number; name: string }[]>(
+        "http://localhost:3000/tags"
+      );
+      setTags(tagsResult.data.map((el) => ({ ...el, checked: false })));
+    };
+    fetchTags();
   }, []);
   return (
     <form
@@ -19,7 +30,14 @@ const NewAd = () => {
         const form = e.target;
         const formData = new FormData(form as HTMLFormElement);
         const formJson = Object.fromEntries(formData.entries());
-        axios.post("http://localhost:3000/ads", formJson);
+        const dataForBackend = Object.assign(formJson, {
+          tags: tags
+            .filter((el) => el.checked === true)
+            .map((el) => {
+              return { id: el.id };
+            }),
+        });
+        axios.post("http://localhost:3000/ads", dataForBackend);
       }}
     >
       <label>
@@ -71,6 +89,33 @@ const NewAd = () => {
           </option>
         ))}
       </select>
+      <br />
+      {tags.map((tag) => (
+        <React.Fragment key={tag.id}>
+          <label>
+            {tag.name}
+            <input
+              type="checkbox"
+              checked={tag.checked}
+              onChange={() => {
+                setTags(
+                  tags.map((prevStateTag) => {
+                    if (prevStateTag.id === tag.id) {
+                      return {
+                        ...prevStateTag,
+                        checked: !prevStateTag.checked,
+                      };
+                    } else {
+                      return prevStateTag;
+                    }
+                  })
+                );
+              }}
+            />
+          </label>
+          <br />
+        </React.Fragment>
+      ))}
       <button className="button">Submit</button>
     </form>
   );
